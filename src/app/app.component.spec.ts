0a9175c8ccba0c18dd8917da09faebe095ca79e7
Alfredo Sirait <edo.sirait25@gmail.com>
2025-10-29 13:34:34 +0700

    upgrade and update code behaviour with systematic, thematic, as well structural code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'personal-profile' title`, () => {
+  it(`should have as title 'personal-profile-clean'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-profile');
+    expect(app.title).toEqual('personal-profile-clean');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, personal-profile');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, personal-profile-clean');
   });
 });
